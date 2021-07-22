e5d569c4df7743b609a6fc31d24d45f2a94e580f
DN-Vanguard <Domiltannguyen@outlook.com>
2021-07-21 17:15:04 -0700

    index.js has functions to initialize app completed.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// packages needed for this application
+// Packages needed for this application
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
+// Created an array of questions for user input
 const questions = [
     {
         type: "input",
@@ -55,15 +55,19 @@ const questions = [
 
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(`./output/${fileName}`, data, (err) => err ? console.error(err) : console.log('Your README file has now been created!'));
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
-
-    
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            const markdownString = generateMarkdown(response);
+            writeToFile("README.md", markdownString);
+        });
 }
 
 // Function call to initialize app
